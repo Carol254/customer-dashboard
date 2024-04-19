91b85b2c9da356e65a4ea689529ef573bcaba72f
Caroline <wanguic49@gmail.com>
2024-04-19 14:55:22 +0300

    few changes made on the nav

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,11 +10,16 @@ export class DashboardComponent  implements OnInit{
   ngOnInit(){
     const button = document.querySelector('#sidebar-toggle');
     const wrapper = document.querySelector('#wrapper');
+    const elements: NodeListOf<Element> = document.querySelectorAll('.side-nav-item');
 
     if (button && wrapper) {
       button.addEventListener('click', (e) => {
         e.preventDefault();
         wrapper.classList.toggle('toggled');
+       elements.forEach((el)=>{
+        el.classList.toggle('toggled');
+       });
+
       });
     }
   }
